0bf4784e9f06c25b3c33ff87af5c7c958cc5e670
zohaib khalid <zohaibchaudry6@gmail.com>
2026-01-09 22:50:44 +0500

    add url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+// const API_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.PROD
+  ? import.meta.env.VITE_API_URL
+  : 'http://localhost:5000/api';
 // Create axios instance with default config
 const api = axios.create({
   baseURL: API_URL,
